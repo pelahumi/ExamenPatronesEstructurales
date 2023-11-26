dfc7713f974514441d29e6e621c8f07cb1fcaf65
Pelayo <phuermij@myuax.com>
2023-11-26 11:00:49 +0100

    cambios

--- Samur/auxiliar.py
+++ Samur/auxiliar.py
@@ -1,11 +1,2 @@
-import sqlite3
+from faker import Faker
 
-db = sqlite3.connect("Samur/DataBase/acceso.db")
-cursor = db.cursor()
-
-def usuario_check(usuario):
-    cursor.execute("SELECT usuario FROM acceso WHERE usuario = ?", (usuario,))
-    if cursor.fetchone():
-        return True
-    else:
-        return False
